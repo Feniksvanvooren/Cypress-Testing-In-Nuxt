e010896d7d757841cd81bc07a31536af80e54718
Feniks Van Vooren <feniks.vanvooren@student.odisee.be>
2023-05-30 23:13:47 +0200

    add files

---
+++ pages/login-form.vue
@@ -0,0 +1,15 @@
+<template>
+  <div class="site-content">
+    <div class="site-wrap container-wrap my-5">
+      <div class="border-radius my-5 col-10 m-auto">
+        <SimpleForm />
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "IndexPage",
+};
+</script>
